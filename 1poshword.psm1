c397ea21ce9228800050e60f0955f0703e4476eb
Lincoln Atkinson <latkin@users.noreply.github.com>
2016-08-21 19:37:40 -0700

    Assortment of cleanup and refactors

--- 1poshword.psm1
+++ 1poshword.psm1
@@ -2,101 +2,63 @@
 Set-StrictMode -Version 2
 $errorActionPreference = 'Stop'
 $isWindows,$isOSX,$isLinux = 
-    if($psVersionTable.PSVersion.Major -ge 6){ $isWindows,$isOSX,$isLinux }
+    if ($psVersionTable.PSVersion.Major -ge 6) { $isWindows,$isOSX,$isLinux }
     else { $true,$false,$false }
 
 $1passwordRoot = "${env:userprofile}\Dropbox\1Password\1Password.agilekeychain\data\default"
 
-function ClipboardCopy
-{
-    param(
-        [string[]] $data
-    )
-
-    if($isWindows) {
-        $data | clip.exe
-    } elseif($isOSX) {
-        $data | pbcopy
-    } elseif($isLinux -and (gcm xclip)) {
-        $data | xclip
-    } else {
-        Write-Error "Unable to locate clipboard utility"
-    }
+function ClipboardCopy([string[]] $Data) {
+    if ($isWindows) { $data | clip.exe }
+    elseif ($isOSX) { $data | pbcopy }
+    elseif ($isLinux -and (gcm xclip)) { $data | xclip }
+    else { Write-Error "Unable to locate clipboard utility" }
 }
 
-function DecodeSaltedString
-{
-    param(
-        [string] $encodedString
-    )
-    
+function DecodeSaltedString([string] $EncodedString) {
     $bytes = [System.Convert]::FromBase64String($encodedString)
-    
-    [PSCustomObject]@{
-        Salt = $bytes[8..15]
-        Data = $bytes[16..($bytes.Length - 1)]
+    [PSCustomObject] @{
+        Salt = $bytes[8 .. 15]
+        Data = $bytes[16 .. ($bytes.Length - 1)]
     }
 }
 
-function DeriveKeyPbkdf2
-{
-    param(
-       [string] $password,
-       [byte[]] $salt,
-       [int] $iters
-    )
-    
-    $deriveBytes = new-object System.Security.Cryptography.Rfc2898DeriveBytes $password,$salt,$iters
+function DeriveKeyPbkdf2([string] $Password, [byte[]] $Salt, [int] $Iterations) {
+    $deriveBytes = New-Object System.Security.Cryptography.Rfc2898DeriveBytes $password,$salt,$iterations
     $keyData = $deriveBytes.GetBytes(32)
-    
-    [PSCustomObject]@{
-        Key = $keyData[0..15]
-        IV = $keyData[16..31]
+    [PSCustomObject] @{
+        Key = $keyData[0 .. 15]
+        IV = $keyData[16 .. 31]
     }
 }
 
-function DeriveKeyOpenSSL
-{
-    param(
-       [byte[]] $key,
-       [byte[]] $salt
-    )
-    
-    $key = $key[0..($key.length - 17)]
+function DeriveKeyMD5([byte[]] $Key, [byte[]] $Salt) {
+    $key = $key[0 .. ($key.length - 17)]
     $md5 = [System.Security.Cryptography.MD5]::Create()
     $prev = @()
     $keyData = @()
-    while($keyData.Length -lt 32)
-    {
+    while ($keyData.Length -lt 32) {
         $prev = $md5.ComputeHash($prev + $key + $salt)
         $keyData += $prev
     }
-    
-    [PSCustomObject]@{
-         Key = $keyData[0..15]
-         IV = $keyData[16..31]
+
+    [PSCustomObject] @{
+         Key = $keyData[0 .. 15]
+         IV = $keyData[16 .. 31]
     }
 }
 
-function AESDecrypt
-{
-    param(
-        [byte[]] $data,
-        [byte[]] $key,
-        [byte[]] $iv
-    )
-    
+function AESDecrypt([byte[]] $Data, [byte[]] $Key, [byte[]] $IV) {
     $aes = [System.Security.Cryptography.Aes]::Create()
     $aes.Key = $key
     $aes.IV = $iv
     $aes.Padding = 'None'
     $decryptor = $aes.CreateDecryptor($key, $iv)
-    $memStream = new-object System.IO.MemoryStream @(,$data)
-    $cryptStream = new-object System.Security.Cryptography.CryptoStream $memStream, $decryptor, 'Read'
+    $memStream = New-Object System.IO.MemoryStream @(,$data)
+    $cryptStream = New-Object System.Security.Cryptography.CryptoStream $memStream,$decryptor,'Read'
 
     $result = $(
         $b = $cryptStream.ReadByte()
-        while($b -ne -1){
+        while ($b -ne -1) {
             $b
             $b = $cryptStream.ReadByte()
         }
@@ -105,131 +67,100 @@ function AESDecrypt
     $memStream.Dispose()
     $decryptor.Dispose()
     $aes.Dispose()
-    
+
     $paddingSize = $result[-1]
-    if($paddingSize -ge 16){
+    if ($paddingSize -ge 16) {
         $result
     } else {
-        $result[0..($result.Length - $paddingSize - 1)]
+        $result[0 .. ($result.Length - $paddingSize - 1)]
     }
 }
 
-function GetDecryptionKey
-{
-    param(
-        [string] $KeyId,
-        [string] $SecurityLevel,
-        [string] $RootDir
-    )
-
+function GetDecryptionKey([string] $KeyId, [string] $SecurityLevel, [string] $RootDir) {
     $keysJson = cat "$rootDir\encryptionKeys.js" | ConvertFrom-Json
-    if($keyId)
-    {
-        $keysJson.list |?{ $_.identifier -eq $keyId }
-    }
-    else
-    {
-        $keysJson.list |?{ $_.level -eq $securityLevel }
+    if ($keyId) {
+        $keysJson.list |? identifier -eq $keyId
+    } else {
+        $keysJson.list |? level -eq $securityLevel
     }
 }
 
-function GetContents
-{
-    param(
-        [string] $RootDir
-    )
-
-    foreach($item in (cat "$rootDir\contents.js" | ConvertFrom-Json))
-    {
-        [PSCustomObject]@{
-            ID = $item[0]
+function GetEntries([string] $RootDir) {
+    foreach($item in (cat "$rootDir\contents.js" | ConvertFrom-Json)) {
+        [PSCustomObject] @{
+            Id = $item[0]
             Name = $item[2]
         }
     }
 }
 
-function GetPayloadFromDecryptedJson
-{
-    param(
-        [string] $Json,
-        [string] $TypeName
-    )
-    
-    $item = $json | ConvertFrom-Json
-    
+function GetPayloadFromDecryptedEntry([string] $EntryJson, [string] $TypeName) {
+    $entry = $entryJson | ConvertFrom-Json
+    $username = $null
+    $password = $null
+    $text = $null
+
     switch($typeName) {
         'webforms.WebForm' {
             Set-StrictMode -Off
-            $password = $item.fields |?{ $_.designation -eq 'password' } |%{ $_.value }
-            $username = $item.fields |?{ $_.designation -eq 'username' } |%{ $_.value }
-            [PSCustomObject] @{
-                Type = $typeName
-                Username = $username
-                Password = $password
-                Text = $null
-            }
+            $password = $entry.fields |? designation -eq 'password' |% value
+            $username = $entry.fields |? designation -eq 'username' |% value
             Set-StrictMode -Version 2
         }
         'passwords.Password' {
-            [PSCustomObject] @{
-                Type = $typeName
-                UserName = $null
-                Password = $item.password
-                Text = $null
-            }
+            $password = $entry.password
         }
         'securenotes.SecureNote' {
-            [PSCustomObject] @{
-                Type = $typeName
-                UserName = $null
-                Password = $null
-                Text = $item.notesPlain
-            }
+            $text = $entry.notesPlain
         }
         default {
             Write-Error "Entry type $typeName is not supported"
-            exit
         }
+    }
+    
+    [PSCustomObject] @{
+        Type = $typeName
+        Username = $username
+        Password = $password
+        Text = $text
     }
 }
 
-function DecryptItem
-{
-    param(
-        [string] $ItemID,
-        [string] $MasterPassword,
-        [string] $RootDir
-    )
-    
-    $itemJson = cat "$rootDir\$itemID.1password" | ConvertFrom-Json
-    
+function Decrypt([string] $Data, [object] $Key, [int] $Iterations, [switch] $MD5, [switch] $Pbkdf2) {
+    $decoded = DecodeSaltedString $data
+    $finalKey =
+        if ($md5) {
+            DeriveKeyMD5 ([byte[]] $key) $decoded.Salt
+        } elseif ($pbkdf2) {
+            DeriveKeyPbkdf2 ([string] $key) $decoded.Salt $iterations
+        }
+
+    AESDecrypt $decoded.Data $finalKey.Key $finalKey.IV
+}
+
+function DecryptEntry([string] $EntryId, [string] $MasterPassword, [string] $RootDir) {
+    $entry = cat "$rootDir\$entryId.1password" | ConvertFrom-Json
+
     Set-StrictMode -Off
-    $keyId = $itemJson.KeyID
-    $securityLevel = $itemJson.securityLevel
+    $keyId = $entry.KeyId
+    $securityLevel = $entry.securityLevel
     Set-StrictMode -Version 2
 
     $decryptionKey = GetDecryptionKey $keyId $securityLevel $rootDir
 
-    $decoded = DecodeSaltedString $decryptionKey.data
-    $keyKey = DeriveKeyPbkdf2 $masterPassword $decoded.Salt $decryptionKey.Iterations
-    $dataKey = AESDecrypt $decoded.Data $keyKey.Key $keyKey.IV
+    $dataKey = Decrypt -Pbkdf2 $decryptionKey.data $masterPassword $decryptionKey.Iterations
+    $dataKeyCheck = Decrypt -MD5 $decryptionkey.validation $dataKey
 
-    $decodedValidation = DecodeSaltedString $decryptionkey.validation
-    $validationKey = DeriveKeyOpenSSL $dataKey $decodedValidation.Salt
-    $finalValidation = AESDecrypt $decodedValidation.Data $validationKey.Key $validationKey.IV
-    if(Compare-Object $finalValidation $dataKey){
+    if (Compare-Object $dataKey $dataKeyCheck) {
         Write-Error "Unable to validate master password"
     }
 
-    $dataDecoded = DecodeSaltedString $itemJson.encrypted
-    $finalKey = DeriveKeyOpenSSL $dataKey $dataDecoded.Salt
-    $finalData = AESDecrypt $dataDecoded.Data $finalKey.Key $finalKey.IV
+    $data = Decrypt -MD5 $entry.encrypted $dataKey
 
-    GetPayloadFromDecryptedJson ([system.text.encoding]::UTF8.GetString($finalData).Trim() -replace '\p{C}+$','') $itemJson.typeName
+    GetPayloadFromDecryptedEntry ([system.text.encoding]::UTF8.GetString($data).Trim() -replace '\p{C}+$','') $entry.typeName
 }
 
-function Set-1PDefaultDirectory
-{
+function Set-1PDefaultDirectory {
     param(
         [Parameter(Mandatory = $true)]
         [ValidateScript({Test-Path $_ -PathType Container})]
@@ -239,13 +170,11 @@ function Set-1PDefaultDirectory
     $script:1PasswordRoot = $Path
 }
 
-function Get-1PDefaultDirectory
-{
+function Get-1PDefaultDirectory {
     $script:1PasswordRoot
 }
 
-function Unprotect-1PEntry
-{
+function Unprotect-1PEntry {
     [CmdletBinding(DefaultParameterSetName = 'plain')]
     param(
         [Parameter(Mandatory = $true, Position = 0)]
@@ -267,59 +196,54 @@ function Unprotect-1PEntry
         [string] $1PasswordRoot = ($script:1PasswordRoot)
     )
 
-    $contents = GetContents $1PasswordRoot
+    $entries = GetEntries $1PasswordRoot
 
-    $item = $contents |?{ $_.Name -like $name }
+    $entry = $entries |? Name -like $name
 
-    if(-not $item)
-    {
+    if (-not $entry) {
         Write-Error "Unable to find entry matching $name"
-        return
     }
 
-    if(@($item).Length -gt 1)
-    {
-        Write-Error "More than one entry matches ${name}: $(($item |% Name) -join ',')"
-        return
+    if (@($entry).Length -gt 1) {
+        Write-Error "More than one entry matches ${name}: $(($entry |% Name) -join ',')"
     }
 
     $plainPass =
-        if($credential -eq $null) {
+        if ($credential -eq $null) {
             $securePass = Read-Host "1Password master password" -AsSecureString
             (New-Object PSCredential @('1poshword', $securePass)).GetNetworkCredential().Password
         } else {
             $credential.GetNetworkCredential().Password
         }
 
-    $decrypted = DecryptItem $item.ID $plainPass $1passwordRoot
+    $decrypted = DecryptEntry $entry.Id $plainPass $1passwordRoot
 
     $result =
         switch($psCmdlet.ParameterSetName) {
             'plain' {
-                if($decrypted.Type -eq 'securenotes.SecureNote') {
-                    $decrypted.Text
-                } else {
-                    $decrypted.Username
-                    $decrypted.Password
-                }
+                $decrypted.Username
+                $decrypted.Password
+                $decrypted.Text
             }
             'passwordonly' {
-                if($decrypted.Password -eq $null) {
+                if ($decrypted.Password -eq $null) {
                     Write-Error "'PasswordOnly' not supported for $($decrypted.Type)"
                 }
+
                 $decrypted.Password
             }
             'ascredential' {
-                if($decrypted.Password -eq $null) {
+                if ($decrypted.Password -eq $null) {
                     Write-Error "'AsCredential' not supported for $($decrypted.Type)"
                 }
+
                 $securePass = New-Object SecureString
                 $decrypted.Password.ToCharArray() |%{ $securePass.AppendChar($_) }
                 New-Object PSCredential @($decrypted.Username, $securePass)
             }
         }
-    
-    if($toClipboard) { ClipboardCopy $result }
+
+    if ($toClipboard) { ClipboardCopy $result }
     else { $result }
 }
 
